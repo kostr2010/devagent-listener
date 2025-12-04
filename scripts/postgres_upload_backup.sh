847a93c01a756576a55b0129ebaa9d789df4acca
Nazarov Konstantin <nazarov.konstantin1@huawei.com>
2025-12-04 23:50:21 +0800

    2025-12-04
    - add script for feedback monitoring
    - fix backup script

--- scripts/postgres_upload_backup.sh
+++ scripts/postgres_upload_backup.sh
@@ -17,6 +17,6 @@ set +a
 BCK_FILE="bck-$(date "+%y-%m-%d-%s").sql"
 readonly BCK_FILE
 
-docker exec -it devagent_listener_postgres pg_dumpall -p "${DB_PORT}" -U "${DB_USER}" -l "${DB_DB}" > "${BCK_FILE}"
+docker exec devagent_listener_postgres pg_dumpall -p "${DB_PORT}" -U "${DB_USER}" -l "${DB_DB}" > "${BCK_FILE}"
 curl --user "${NEXUS_USERNAME}":"${NEXUS_PASSWORD}" -X PUT "${NEXUS_REPO_URL}/${BCK_FILE}" -T "${BCK_FILE}"
 rm "${BCK_FILE}"
